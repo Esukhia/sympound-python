4825d876b81d95f7e07783f68d3e0f9800b40e1b
Thubtenrigzin <thubtenrigzin@gmail.com>
2018-06-17 00:36:46 +0800

    some tests on Tibetan

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 import os
-from sympound.sympound import sympound
 
-import time
+from sympound.sympound import sympound
 
 import platform
 distancefun = None
@@ -12,27 +11,21 @@ else:
     from jellyfish import levenshtein_distance
     distancefun = levenshtein_distance
 
-def timeit(method):
-    def timed(*args, **kw):
-        ts = time.time()
-        result = method(*args, **kw)
-        te = time.time()
-        print('%r (%r, %r) %2.2f sec' % (method.__name__, args, kw, te-ts))
-        return result
-    return timed
 
 ssc = sympound(distancefun=distancefun, maxDictionaryEditDistance=3)
 
-#@profile
-@timeit
 def test():
     ssc.load_dictionary(os.path.abspath("example-dict1.txt"))
     print(ssc.lookup_compound(input_string="bonjur bonjour", edit_distance_max=2))
-    #print(ssc.dictionary.get("bonjour")+"\n")
+    print()
     print(ssc.lookup_compound(input_string="bonjuor", edit_distance_max=2))
     print()
     print(ssc.load_dictionary("example-dict2.txt", term_index=0, count_index=1))
     print(ssc.lookup_compound(input_string="bonjur hallo", edit_distance_max=2))
     print()
+    ssc.load_dictionary(os.path.abspath("tibetan.txt"))
+    ssc.save_pickle("tibetan.pickle")
+    #ssc.load_pickle("tibetan.pickle")
+    print(ssc.lookup_compound(input_string="བཀྲ་ཤས་བད་ལེགས། ལ་མ་", edit_distance_max=3))
 
 test()
